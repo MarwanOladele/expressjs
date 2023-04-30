a7d56bd23a26c8dd7f74ad342db0baa8a2cf1675
MarwanOladele <oladelemarwan@gmail.com>
2023-04-30 22:47:22 +0100

    expressjs basics

--- 05-nodejs-custom-core-modules/app.js
+++
@@ -1,6 +0,0 @@
-const store = require("./database/store/dataStore");
-const utils = require("./utils/writeFile");
-
-store.readFile((employee) => {
-  utils.writeFile("data.json", JSON.stringify(employee));
-});
--- 08-nodejs-npm-module-dotenv/app.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "./config/.env" });
-
-const secretKey = process.env.JWT_SECRET_KEY;
-const localHost = process.env.LOCAL_HOST;
-
-console.log(secretKey);
-console.log(localHost);
