63251308a3c5805a741036af594088685c4635ce
MarwanOladele <oladelemarwan@gmail.com>
2023-05-20 19:28:50 +0100

    simple node project

--- 1-express-js/app.js
+++ 1-express-js/app.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const app = express();
-const logger = require('./logger')
-const authorize = require('./authorize')
 
+const { people } = require("./data");
 
-app.use([logger, authorize])
+app.use(express.static("./methods-public"));
 
+// To parse form data
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Home')
-})
+// To parse json
+app.use(express.json());
 
-app.get('/about', (req, res) => {
-  res.send('About')
-})
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
 
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(404).json({ success: false, msg: "imput a value" });
+  }
+  res.status(201).json({ sucess: true, person: [...people, {id: 7, name}] });
+});
 
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('Items')
-})
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please input a name");
+});
 
 app.listen(5000, () => {
   console.log("Server listening on port 5000...");
